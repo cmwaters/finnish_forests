1b0c217cad2dbbc97287864f7a709b7796f00b80
Callum Michael Waters <c.waters@campus.tu-berlin.de>
2019-11-16 14:25:42 +0200

    added dashboard connectability

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var router = express.Router();
-const data = require('./data');
+const express = require('express');
+const router = express.Router();
+const RouteModel = require('../models/routes');
 
-router.get('/', function(req, res) {
-    res.render('dashboard', {title: 'Dashboard'})
+router.get('/', async (req, res) => {
+    try {
+        const routes = await RouteModel.find();
+        res.render('dashboard', {routes: routes})
+    } catch (e) {
+        res.status(500).json({ msg: e.message })
+    }
 });
 
-router.get('/routes', function(req, res) {
-    res.render('dashboard', {title: 'Dashboard routes', routes: data})
-});
-
-router.get('/routes/:id', function(req, res) {
-    res.send(req.params.id);
+router.post('/', async (req, res) => {
+    try {
+        const route = new RouteModel({
+            name: req.body.route.name
+        });
+        route.save();
+        res.redirect(req.originalUrl)
+    } catch (e) {
+        res.status(400).json({ msg: e.message });
+    }
 });
 
 module.exports = router;
